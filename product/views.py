c0404fc2109aa77502a6917beb3d8dff238b642b
Vitor Hugo Campos <vitorh45@gmail.com>
2015-07-01 09:12:33 -0300

    Listing and showing the products list and product detail

--- product/views.py
+++ product/views.py
@@ -3,10 +3,15 @@
 from django.views.generic import ListView, DetailView
 from product.models import Product
 
-class ProductList(ListView):
+class ProductListView(ListView):
     model = Product
     context_object_name = 'product_list'
 
 
-class ProductDetail(DetailView):
+class ProductDetailView(DetailView):
     model = Product
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(**kwargs)
+        return context
